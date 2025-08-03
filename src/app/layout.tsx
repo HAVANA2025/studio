8e49cf47532c50abc7ffb83cda8b5f5da7598cea
web dev <webdevlopment0210@gmail.com>
2025-08-03 11:03:25 +0000

    yes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
-import { Inter } from 'next/font/google';
+import { Inter, Audiowide } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'G-Electra',
@@ -16,6 +16,12 @@ const inter = Inter({
   variable: '--font-inter',
 });
 
+const audiowide = Audiowide({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-audiowide',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -23,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={cn('font-body antialiased min-h-screen bg-background flex flex-col', inter.variable)}>
+      <body className={cn('font-body antialiased min-h-screen bg-background flex flex-col', inter.variable, audiowide.variable)}>
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
