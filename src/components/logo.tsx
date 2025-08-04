12285895c65edabd7e8de4c0cb2edafce6252815
web dev <webdevlopment0210@gmail.com>
2025-08-04 19:37:46 +0000

    bro my logo is in images folder which is in public folder logo.png

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
     return (
         <div className={cn("relative", className)}>
             <Image
-                src="/favicon.ico"
+                src="/images/logo.png"
                 alt="G-Electra Logo"
                 fill
                 className="object-contain"
