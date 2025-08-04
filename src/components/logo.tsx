1ff876277264c9c03cea19a1b65ce535a9d92f69
web dev <webdevlopment0210@gmail.com>
2025-08-04 19:35:26 +0000

    even in incognito tab old favicon is being displayed there must be an is

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
     return (
         <div className={cn("relative", className)}>
             <Image
-                src="/images/logo.png"
+                src="/favicon.ico"
                 alt="G-Electra Logo"
                 fill
                 className="object-contain"
