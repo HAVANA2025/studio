ab34f4a6821ebac8547903663a2593fa2ac2774a
web dev <webdevlopment0210@gmail.com>
2025-08-05 20:51:21 +0000

    just some more to left and we are perfect to go

--- src/components/floating-cta.tsx
+++ src/components/floating-cta.tsx
@@ -14,7 +14,7 @@ export function FloatingCta() {
 
   return (
     <>
-      <div className="fixed bottom-6 right-6 z-50">
+      <div className="fixed bottom-6 left-6 z-50">
         <div className="relative group">
           <div className="absolute -inset-1.5 bg-gradient-to-r from-primary to-accent rounded-lg opacity-75 blur-lg group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
           <button
