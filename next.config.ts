e264c6ca9ccdbadf9ebde7d4db537085d43c8525
web dev <webdevlopment0210@gmail.com>
2025-08-12 17:57:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  }
 };
 
 export default nextConfig;
